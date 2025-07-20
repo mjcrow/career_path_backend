32d676df5471846ccd84ff26708382040093fc6c
mjcrow <mjcrow001@gmail.com>
2025-07-20 00:10:24 -0500

    Bundle React properly by removing externals

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,12 +23,6 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'react-dom/client',
-        'react-router'
-      ],
     },
   },
 });
